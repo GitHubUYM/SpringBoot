5b9f04856ae5e50dbf83a45999cdd385ebcfda51
GitHubUYM <uypingping@163.com>
2023-04-13 15:37:54 +0800

    SpringBoot

---
+++ SpringBoot/demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Demo1Application {
+	private static final Logger log = LoggerFactory.getLogger(Demo1Application.class);
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://localhost:8080/api/random", Quote.class);
+			assert quote != null;
+			log.info(quote.toString());
+		};
+	}
+
+}
